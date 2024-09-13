4c8edf75d15c90b8ee56da9cfb068090b69007c1
Elvin Gamecraft <elvingamecraft@hotmail.com>
2024-09-13 10:29:22 +0200

    Page 7 fini

--- Exercices/Page_7/P7_Ex4.py
+++ Exercices/Page_7/P7_Ex4.py
@@ -1,7 +1,10 @@
 # Code par Elvin Mouyart 5Tb
-Fini = False
+Fini = True
 # P7_Ex4
 
 larg = int(input("Introduisez la largeur svp > "))
 haut = int(input("Introduisez la hauteur svp > "))
+print("X" * larg)
+for i in range(haut-2):
+    print("X" + " " * (larg - 2) + "X")
 print("X" * larg)
--- Exercices/Page_7/P7_Ex5.py
+++ Exercices/Page_7/P7_Ex5.py
@@ -1,3 +1,11 @@
 # Code par Elvin Mouyart 5Tb
-Fini = False
+Fini = True
 # P7_Ex5
+
+import random
+count = 0
+nbr = 0
+while (nbr != 75):
+    nbr = random.randint(1,101)
+    count+=1
+print(f"Il aurat fallut {count} essai(s) pour obtenir le nbr {nbr}")
